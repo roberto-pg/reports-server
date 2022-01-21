1e138d485c185d6d4b073d43ebaada39a7d378f9
Roberto <rpg61@hotmail.com>
2022-01-21 13:14:26 -0300

    Create the database

---
+++ src/infra/prisma/migrations/20220121160842_create_db/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "reports" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "initial_description" TEXT NOT NULL,
+    "initial_image" TEXT NOT NULL,
+    "started_at" TIMESTAMP(3) NOT NULL,
+    "final_description" TEXT,
+    "final_image" TEXT,
+    "stoped_at" TIMESTAMP(3),
+    "finished" BOOLEAN DEFAULT false,
+
+    CONSTRAINT "reports_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "reports" ADD CONSTRAINT "reports_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
